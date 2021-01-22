d54105820aeb2facaea7884965321a826988aeb0
Jo <joembedded@gmail.com>
2021-01-22 14:56:22 +0100

    pin

--- legacy/gen_key_badge.php
+++ legacy/gen_key_badge.php
@@ -131,11 +131,10 @@ if (!file_exists(S_DATA . "/$mac") || check_dirs()) exit_error("Error (Directory
 	}
 
 	$of = fopen($dpath . 'def_factory_key.php', 'w');	// Cache Factory Key
-	fwrite($of, "<?php\n define(\"FW_FACTORY_KEY\",\"" . implode(unpack("H*", $sec_key)) . "\");\n?>\n");
+	fwrite($of, "<?php\n define(\"FW_FACTORY_KEY\",\"" . implode("",unpack("H*", $sec_key)) . "\");\n?>\n");
 	fclose($of);
 
-
-	$fw_key = strtoupper(implode(unpack("H*", $sec_key)));
+	$fw_key = strtoupper(implode("",unpack("H*", $sec_key)));
 	$script = $_SERVER['PHP_SELF'];	// /xxx.php
 	$server = $_SERVER['HTTP_HOST'];
 	$lp = strpos($script, "legacy/gen_key");
@@ -151,7 +150,10 @@ if (!file_exists(S_DATA . "/$mac") || check_dirs()) exit_error("Error (Directory
 	$res = "";
 	for ($i = 0; $i < 6; $i++) $res .= chr(ord($plain[$i]) ^ (($i * 15 + $xc) & 255));
 	$res .= chr(($crc >> 8) & 255) . chr($xc);
-	$ticket = strtoupper(implode(unpack("H*", $res)));	// String 2 Hex-String, reverse: pack("H*", $hex);
+	$ticket = strtoupper(implode("",unpack("H*", $res)));	// String 2 Hex-String, reverse: pack("H*", $hex);
+
+	// Pin: get a 6-digit PIN 100100-999899 out of MAC
+	$pin=((((~crc32(pack("V*",hexdec(substr($mac,0,8)),hexdec(substr($mac,8,8))))) & 0xFFFFFFFF))%899800)+100100;	
 
 	$anz = 1;	// Number of Badges
 	echo "<br><br><hr><br>";
@@ -161,8 +163,9 @@ if (!file_exists(S_DATA . "/$mac") || check_dirs()) exit_error("Error (Directory
 	$qrlink = "../sw/php_qr/ltx_qr.php?text=" . urlencode($qrtxt) . "&px=3&fx=1";
 	for ($i = 0; $i < $anz; $i++) {
 		echo "<table>";
-		echo "<tr style='font-size:120%; font-weight:bold;'><td>Device MAC:</td><td>$mac</td><td rowspan='3' style='margin:0; padding:0;' ><img src='$qrlink'></td></tr>";
+		echo "<tr style='font-size:120%; font-weight:bold;'><td>Device MAC:</td><td>$mac</td><td rowspan='4' style='margin:0; padding:0;' ><img src='$qrlink'></td></tr>";
 		echo "<tr style='font-size:100%; font-weight:bold;'><td>Device Owner Token:</td><td>$ownertoken</td></tr>";
+		echo "<tr style='font-size:100%; font-weight:bold;'><td>Device PIN:</td><td>$pin</td></tr>";
 		echo "<tr><td>Server Login$sec:</td><td>" . $server . $sroot . "</td></tr>";
 		echo "<tr><td>Server Ticket:</td><td>$ticket</td><td valign='top'  style='font-size:60%;'> MAC/OwnerToken</td></tr>";
 		echo "</table><br>";
