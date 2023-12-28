f82436b71923374dd63f6cc20380b7d66a8f4508
Jo <joembedded@gmail.com>
2023-12-28 20:07:26 +0100

    Cleanup

--- sw/lxu_loglib.php
+++ sw/lxu_loglib.php
@@ -5,7 +5,7 @@
 // ---- basic directory service ---
 function check_dirs($wcmd = true)
 {
-	global $xlog, $dbg, $mac, $dapikey;
+	global $dbg, $mac, $dapikey;
 	$sdata = S_DATA;
 
 	$newdev = false;
@@ -18,7 +18,6 @@ function check_dirs($wcmd = true)
 		mkdir($sdata . "/$mac");
 		file_put_contents($sdata . "/$mac/date0.dat", time()); // Note initial date
 		file_put_contents($sdata . "/$mac/quota_days.dat", DB_QUOTA); 
-		file_put_contents($sdata . "/$mac/dapikey.dat", $dapikey); 
 		$newdev = $wcmd;
 	}
 	if (!file_exists($sdata . "/$mac/cmd")) {
