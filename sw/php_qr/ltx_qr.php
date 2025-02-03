5bb5a6aa686fc9f4e2397dc752676634a605bac0
Jo <joembedded@gmail.com>
2025-02-03 14:20:00 +0100

    VersionTracker

--- sw/php_qr/ltx_qr.php
+++ sw/php_qr/ltx_qr.php
@@ -1,10 +1,10 @@
 <?php 
-// QR-Generator PHP - Stand 25.01.25
+// QR-Generator PHP - Stand 03.02.25
 
 // Quelle: https://www.geeksforgeeks.org/dynamically-generating-a-qr-code-using-php/
 // Include the qrlib file 
-	include '../php_qr/qrlib.php'; // selbiges Verzeichnis
-  
+include '../php_qr/qrlib.php'; // selbiges Verzeichnis
+
 // $text variable has data for QR  
 // $text = "geo:20.33470,20.39448"; 
 // $text = "https://joembedded.de/ltx";
@@ -16,22 +16,22 @@
 // Test: http://localhost/ltx/sw/php_qr/ltx_qr.php?text=Hallo%20Welt&ecc=M
 // Test: https://joembedded.de/ltx/sw/php_qr/ltx_qr.php?text=Hallo%20Welt&ecc=M
 
+//error_reporting(E_ALL); - Weglassen, sonst lange error-Datei!
+define("VERSION", "V1.00 / 03.02.2025");
 
-  // Parameters:
-  $text=@$_REQUEST['text'];
-  $ecc =@$_REQUEST['ecc'] ; // L M Q H mgl.
-  $pixel_Size = @$_REQUEST['px']; // Pixel Groessen
-  $frame_Size = @$_REQUEST['fx'];
-  
-  if(!strlen($text)) $text="(NoText)";
-  if(!strlen($ecc)) $ecc = 'L' ; // L M Q H mgl.
-  if($pixel_Size<1)  $pixel_Size = 5; // Pixel Groessen
-  if($frame_Size<1) $frame_Size = 5; 
- 
-  //echo "Text:'$text', Ecc:'$ecc', PixelSize:$pixel_Size, FrameSize:$frame_Size\n"; exit(); // DBG
- 
-  header('Access-Control-Allow-Origin: *');
+// Parameters:
+$text = $_REQUEST['text'] ?? '(NoText)';
+$ecc = $_REQUEST['ecc'] ?? 'L'; // L M Q H mgl.
+$pixel_Size = (int)($_REQUEST['px'] ?? 5); // Pixel Groessen
+$frame_Size = (int)($_REQUEST['fx'] ?? 5);
 
-  // Generates QR Code and NOT Stores it
-  QRcode::png( $text , false , $ecc , $pixel_Size , $frame_Size ); 
+if ($pixel_Size < 1) $pixel_Size = 5; // Pixel Groessen
+if ($frame_Size < 1) $frame_Size = 5;
+
+//echo "Text:'$text', Ecc:'$ecc', PixelSize:$pixel_Size, FrameSize:$frame_Size\n"; exit(); // DBG
+
+header('Access-Control-Allow-Origin: *');
+
+// Generates QR Code and NOT Stores it
+QRcode::png($text, false, $ecc, $pixel_Size, $frame_Size); 
 ?>
